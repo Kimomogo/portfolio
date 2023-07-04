3c4a72d6c3c0501ad4b43594f81e6b938829bf9e
Kimomogo <136183441+Kimomogo@users.noreply.github.com>
2023-07-03 20:26:50 -0700

    Add files via upload

---
+++ ask.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="ask.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio-Kidus Moges</title>
+    <header>
+        <div class="logo">
+           <a href="index.html" style="color:black ;"> KIDUS MOGES</a>
+        </div>
+        <div class="links">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="work.html">Work</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="about.html">About</a></li>
+        </div>
+        <div class="search">
+            <form action="https://www.google.com" method="get">
+                <input type="text" name="q" id="">
+                <input type="submit" value="Search">
+            </form>
+        </div>
+    </header>
+  <hr>
+</head>
+<body>
+    <div class="main">
+  <form action="ask.html" method="post">
+          <input type="text" name="text" id="text">
+          <input type="submit" value="Ask">
+          <p id="media"></p>
+  </div>
+  </form>
+</body>
+</html><br><br><br><br><hr>
+<footer>
+<span>
+    <h2>Social media</h2>
+    <a href="">Dribble</a>
+    <a href="">Github</a>
+    <a href="">Upwork</a>
+    <a href="">LinkedIn</a>
+</span>
+<span>
+    <h2>Pages</h2>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="about.html">Contact</a>
+    <a href="work.html">Work</a>
+</span>
+<span>
+    <h2>Skils</h2>
+    <a href="">Web-developer</a>
+    <a href="">Data entry</a>
+    <a href="">Web-design</a>
+</span>
+
+
+</footer><br><br><br><br>
+<article>All rights reserved.</article>
+<script>
+    if(document.getElementById("text").value = "Hello"){
+     document.getElementById("media").innerhtml;
+    }
+</script>
